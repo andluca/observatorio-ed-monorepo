8d4d20927b50f3a983a0b789bfab638bfee6012c
andluca <109103844+andluca@users.noreply.github.com>
2025-12-24 22:06:57 -0300

    chore: update dependencies and configuration files
    - Updated package.json to change lint command to "next lint" and added postinstall script for Prisma generation.
    - Added Prisma seed script configuration.
    - Updated dependencies including Next.js, React, and Tailwind CSS.
    - Removed postcss.config.mjs and created postcss.config.js for PostCSS configuration.
    - Added tailwind.config.js for Tailwind CSS configuration.
    - Modified tsconfig.json to adjust library options, JSX setting, and include/exclude patterns.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  experimental: {
+    // reactCompiler: true, // Descomente apenas se tiver certeza que quer usar o compilador experimental
+  },
 };
 
 export default nextConfig;
